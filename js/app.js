80fc52d380b6ebabf23e11727a540c919fa19970
Janos Kocs <janos.kocs@outlook.com>
2022-05-23 23:05:29 +0100

    Initial page and comments

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const multiStepForm = document.querySelector('[data-multi-step]')
-const formSteps = [...multiStepForm.querySelectorAll('[data-step]')]
+const multiStepForm = document.querySelector('[data-multi-step]')//ACCESS FORM
+const formSteps = [...multiStepForm.querySelectorAll('[data-step]')]//CREATE AN ARRAY FROM HTML NODES
 
-let currentStep = formSteps.findIndex(step => {
+let currentStep = formSteps.findIndex(step => { //FIND INDEX NEEDS AN ARRAY TO RETURN THE CORRECT STEP WHICH HAS THE CLASS ACTIVE
     return step.classList.contains('active')
 })
 
-if (currentStep < 0) {
-    currentStep = 0
+if (currentStep < 0) {//FIND INDEX RETURNS -1 IF NO ELEMENT HAS ACTIVE CLASS
+    currentStep = 0 //SET THE CURRENT STEP TO 0 WHICH POINTS TO THE 1ST PAGE AND ADD THE CLASS ACTIVE TO IT TO MAKE IT VISIBLE BY DEFAULT
     formSteps[currentStep].classList.add('active');
 }
